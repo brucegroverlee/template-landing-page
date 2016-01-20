1a4eac5f48be6c825016c540a171d1af1bf39a7b
Bruce Grover Lee <brucegrover.lee@gmail.com>
2016-01-20 17:31:59 -0500

    navbar works ok

--- src/frontend/js/indexNavbar.js
+++ src/frontend/js/indexNavbar.js
@@ -1,5 +1,36 @@
 (function () {
-	$('#btnMenu').on('click', function () {
-		$('#navbarMenu').toggleClass('header-menu-list-show')
-	})
+  $('#btnMenu').on('click', function () {
+    $('#navbarMenu').toggleClass('header-menu-list-show')
+  })
+
+  var time = 400
+  var margin_top = $('#view-0').height()
+
+  $('#a-view-1').on('click', function () {
+    $('body,html').animate({
+      scrollTop: $('#view-1').position().top - margin_top
+    }, time)
+    $('#navbarMenu').toggleClass('header-menu-list-show')
+  })
+
+  $('#a-view-2').on('click', function () {
+    $('body,html').animate({
+      scrollTop: $('#view-2').position().top - margin_top
+    }, time)
+    $('#navbarMenu').toggleClass('header-menu-list-show')
+  })
+
+  $('#a-view-3').on('click', function () {
+    $('body,html').animate({
+      scrollTop: $('#view-3').position().top - margin_top
+    }, time)
+    $('#navbarMenu').toggleClass('header-menu-list-show')
+  })
+
+  $('#a-view-4').on('click', function () {
+    $('body,html').animate({
+      scrollTop: $('#view-4').position().top - margin_top
+    }, time)
+    $('#navbarMenu').toggleClass('header-menu-list-show')
+  })
 })()
